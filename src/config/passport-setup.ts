61b74f153d8679671b4e9ae8e309ff33b0a1f660
Jay Wu <jay.wu@item.com>
2024-05-04 16:54:58 +0800

    feat:  serializing Users

--- src/config/passport-setup.ts
+++ src/config/passport-setup.ts
@@ -5,6 +5,18 @@ import User from "../models/user.model"
 
 const GoogleStrategy = GoogleOAuth2.Strategy;
 
+passport.serializeUser((user,done)=>{
+    // 调用这个函数，这个函数会自动的把id传递到某个地方，这个地方会把id放到cookie里面
+    done(null,user.id)
+})
+
+passport.deserializeUser((id,done)=>{
+    // 当用户下次访问我们的网站的时候，我们会拿到cookie里面的id，去找到这个用户
+    User.findById(id).then((user)=> {
+        done(null, user) // 找到后把这个用户传递到下个stage
+    })
+})
+
 const googleStrategy = new GoogleStrategy({
     // options for the google strategy
     // 使用google api 对我们网站上的人员进行身份验证
@@ -19,6 +31,8 @@ const googleStrategy = new GoogleStrategy({
         if (record) {
             // already have the user
             console.log('user is:', record)
+            // 传递给 serializeUser
+            done(null,record)
         } else {
             // if not, create user in our db
             // done(null, profile)
@@ -27,7 +41,7 @@ const googleStrategy = new GoogleStrategy({
                 googleId: profile.id
             }).save().then((newUser) => {
                 console.log('new user created: ', newUser)
-                // done(null, newUser)
+                done(null, newUser) // when we call done, it will call serializeUser
             })
         }
     })
