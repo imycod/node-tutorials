67bbaa479376fa438f9e8303857db22c7ad4ea59
Jay Wu <jay.wu@item.com>
2024-05-04 15:55:43 +0800

    feat: Making a User Model

---
+++ src/models/user.model.ts
@@ -0,0 +1,12 @@
+import moogoose from "mongoose";
+
+const Schema = moogoose.Schema
+
+const userSchema = new Schema({
+    username: String,
+    googleId: String
+})
+
+const User =moogoose.model('user',userSchema);
+
+export default User;
