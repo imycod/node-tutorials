b65d90d2ad5c6f1bbb5a1786e90a38db712994c4
Jay Wu <jay.wu@item.com>
2024-05-04 15:10:49 +0800

    feat: 支持读取环境

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
 import express, {Express, Request, Response} from 'express';
 import path from 'path';
 import authRoutes from "./routes/auth-routes";
+import("./config/passport-setup");
+import {configDotenv} from "dotenv";
+
+configDotenv(); // require('dotenv').config() | dotenv.config() by default
 
 const app: Express = express()
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../src/views')); // 执行的目录是dist，因此需要回退到src目录
 app.use(express.static(path.join(__dirname, '../src/views'))); // 执行的目录是dist，因此需要回退到src目录
-console.log('__dirname---',__dirname)
 
 app.use('/auth', authRoutes);
 app.get('/', (req: Request, res: Response) => {
     res.render('home')
 })
-
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
